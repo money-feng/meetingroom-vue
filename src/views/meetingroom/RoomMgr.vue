28d7a3d8ada4e71cb434e81299bb75252d628274
money <quaeror@126.com>
2020-05-12 17:29:31 +0800

    会议室修改

--- src/views/meetingroom/RoomMgr.vue
+++ src/views/meetingroom/RoomMgr.vue
@@ -7,45 +7,27 @@
       <el-breadcrumb-item>房间列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <el-table
-        :data="meetingRoomLists"
-        border
-        stripe
-      >
+      <el-table :data="meetingRoomLists" border stripe>
         <el-table-column type="expand">
           <template slot-scope="scope">
             <el-row>
-              <el-col :span="12">
+              <el-col :span="12" class="vcenter">
                 <el-col :span="6">
-                  <span>包含设备</span>
+                  <el-tag>包含设备</el-tag>
+                  <i class="el-icon-caret-right"></i>
                 </el-col>
                 <el-col :span="18">
-                  <el-tag
-                    closable
-                    :type="equipments_type[index]"
-                    effect="dark"
-                    v-for="(item, index) in scope.row.equipment_name"
-                    :key="index"
-                    @close="handleClose(scope.row, index)"
-                  >
-                    {{item}}
+                  <el-tag closable :type="equipments_type[index]" effect="dark"
+                    v-for="(item, index) in scope.row.equipment_name" :key="index"
+                    @close="handleClose(scope.row, index)">
+                    {{item.name}}
                   </el-tag>
-                  <el-input
-                    class="input-new-tag"
-                    v-if="addEquipmentVisible"
-                    v-model="addEquipmentValue"
-                    ref="addEquipmentRef"
-                    size="small"
-                    @keyup.enter.native="handleAddEquipmentInputConfirm(scope)"
-                    @blur.native.capture="handleAddEquipmentInputConfirm(scope)"
-                  >
+                  <el-input class="input-new-tag" v-if="addEquipmentVisible[scope.$index]" v-model="addEquipmentValue"
+                    ref="addEquipmentRef" size="small" @keyup.enter.native="handleAddEquipmentInputConfirm(scope)"
+                    @blur.native.capture="handleAddEquipmentInputConfirm(scope)">
                   </el-input>
-                  <el-button
-                    v-else
-                    class="button-new-tag"
-                    size="small"
-                    @click="addEquipment"
-                  >添加设备</el-button>
+                  <el-button v-else class="button-new-tag" size="small" @click="addEquipment(scope.$index)">添加设备
+                  </el-button>
                 </el-col>
 
               </el-col>
@@ -57,179 +39,172 @@
             </el-row>
           </template>
         </el-table-column>
-        <el-table-column
-          label="#"
-          type="index"
-        ></el-table-column>
-        <el-table-column
-          prop="name"
-          label="会议室名称"
-        ></el-table-column>
-        <el-table-column
-          prop="site"
-          label="会议室位置"
-        ></el-table-column>
-        <el-table-column
-          prop="nums"
-          label="容纳人数"
-        ></el-table-column>
-        <el-table-column
-          prop="room_status"
-          label="会议室状态"
-        ></el-table-column>
-        <el-table-column
-          prop="add_time"
-          label="添加时间"
-          :formatter="dateFormat"
-        ></el-table-column>
-        <el-table-column
-          prop="manager"
-          label="录入者"
-        ></el-table-column>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column prop="name" label="会议室名称"></el-table-column>
+        <el-table-column prop="site" label="会议室位置"></el-table-column>
+        <el-table-column prop="nums" label="容纳人数"></el-table-column>
+        <el-table-column prop="room_status" label="会议室状态"></el-table-column>
+        <el-table-column prop="add_time" label="添加时间" :formatter="dateFormat"></el-table-column>
+        <el-table-column prop="manager" label="录入者"></el-table-column>
       </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
-import {
-  request
-} from '@/plugins/http'
-export default {
-  name: "RoomMgr",
-  data () {
-    return {
-      meetingRoomLists: [],
-      // 设备标签样式
-      equipments_type: {
-        0: 'success',
-        1: 'info',
-        2: 'warning',
-        3: 'danger',
-        4: 'primary',
-        5: 'default',
-        6: 'info',
-        7: 'danger',
-        8: 'warning',
-        9: 'success',
-        10: 'primary',
-        11: 'default',
-        12: 'success',
-        13: 'info',
-        14: 'warning',
-        15: 'danger',
-        16: 'primary',
-        17: 'default',
-        18: 'info',
-        19: 'danger',
-        20: 'warning',
-        21: 'success',
-        22: 'primary',
-        23: 'default',
+  import {
+    request
+  } from '@/plugins/http'
+  export default {
+    name: "RoomMgr",
+    data() {
+      return {
+        meetingRoomLists: [],
+        // 设备标签样式
+        equipments_type: {
+          0: 'success',
+          1: 'info',
+          2: 'warning',
+          3: 'danger',
+          4: 'primary',
+          5: 'default',
+          6: 'info',
+          7: 'danger',
+          8: 'warning',
+          9: 'success',
+          10: 'primary',
+          11: 'default',
+          12: 'success',
+          13: 'info',
+          14: 'warning',
+          15: 'danger',
+          16: 'primary',
+          17: 'default',
+          18: 'info',
+          19: 'danger',
+          20: 'warning',
+          21: 'success',
+          22: 'primary',
+          23: 'default',
 
-      },
-      addEquipmentVisible: false,
-      addEquipmentValue: ''
-    }
-  },
-  created () {
-    this.getMeetingRoomLists()
-  },
-  methods: {
-    getMeetingRoomLists () {
-      request({
-        method: "get",
-        url: "/meeting/"
-      }).then(res => {
-        const {
-          data,
-          message,
-          status
-        } = res.data
-        if (status !== 200) return this.handlerNotic(message, 'error')
-        this.meetingRoomLists = data
-      }).catch(err => {
-        this.handlerNotic("连接服务器异常", 'error')
-      })
-    },
-    handlerNotic (message, type) {
-      this.$message({
-        message: message,
-        type: type,
-        center: true
-      });
-    },
-    dateFormat (row, column, cellValue, index) {
-      var datetime = cellValue.split('T')
-      return datetime[0] + " " + datetime[1].split('.')[0]
-    },
-    // 删除会议室设备的函数
-    handleClose (infos, index) {
-      // 使用切片将需要删除到设备清理掉
-      infos.needDel = index
-
-      // 提交数据到后台
-      request({
-        method: "put",
-        url: "/meeting/" + infos.id,
-        data: infos
-      }).then(res => {
-        console.log(res);
-        // infos.equipment_name.splice(index, 1)
-      })
-    },
-
-    addEquipment () {
-      this.addEquipmentVisible = true;
-      this.$nextTick(_ => {
-        this.$refs.addEquipmentRef.$refs.input.focus();
-      });
-    },
-    //   添加会议室设备的函数
-    handleAddEquipmentInputConfirm (infos) {
-      let inputValue = this.addEquipmentValue;
-      if (inputValue) {
-        this.meetingRoomLists[infos.$index].equipment_name.push(inputValue);
+        },
+        addEquipmentVisible: [],
+        addEquipmentValue: ''
       }
-      this.addEquipmentVisible = false;
-      this.addEquipmentValue = '';
+    },
+    created() {
+      this.getMeetingRoomLists()
+    },
+    methods: {
+      getMeetingRoomLists() {
+        request({
+          method: "get",
+          url: "/meeting/"
+        }).then(res => {
+          const {
+            data,
+            message,
+            status
+          } = res.data
+          if (status !== 200) return this.handlerNotic(message, 'error')
+          this.meetingRoomLists = data
+          for (var index = 0; index < this.meetingRoomLists.length; index++) {
+            this.addEquipmentVisible[index] = false
+          }
+        }).catch(err => {
+          this.handlerNotic("连接服务器异常", 'error')
+        })
+      },
+      handlerNotic(message, type) {
+        this.$message({
+          message: message,
+          type: type,
+          center: true
+        });
+      },
+      dateFormat(row, column, cellValue, index) {
+        var datetime = cellValue.split('T')
+        return datetime[0] + " " + datetime[1].split('.')[0]
+      },
+      // 删除会议室设备的函数
+      handleClose(infos, index) {
+        // 提交数据到后台
+        console.log(infos);
+
+        request({
+          method: "put",
+          url: "/meeting/" + infos.id,
+          data: infos
+        }).then(res => {
+          const {
+            data,
+            message,
+            status
+          } = res.data
+          if (status !== 200) return this.handlerNotic(message, 'error')
+          infos.equipment_name.splice(index, 1)
+        })
+        return false
+      },
+
+      addEquipment(index) {
+        this.$set(this.addEquipmentVisible, index, true)
+        this.$nextTick(_ => {
+          this.$refs.addEquipmentRef.$refs.input.focus();
+        });
+      },
+      //   添加会议室设备的函数
+      handleAddEquipmentInputConfirm(infos) {
+        let inputValue = this.addEquipmentValue;
+        if (inputValue) {
+          this.meetingRoomLists[infos.$index].equipment_name.push(inputValue);
+        }
+        this.$set(this.addEquipmentVisible, infos.$index, false)
+        this.addEquipmentValue = '';
+      }
     }
   }
-}
 
 </script>
 
 <style scoped>
-.el-breadcrumb {
-  margin-bottom: 15px;
-  font-size: 13px;
-}
+  .el-breadcrumb {
+    margin-bottom: 15px;
+    font-size: 13px;
+  }
 
-.el-table {
-  margin-top: 20px;
-  font-size: 13px;
-}
+  .el-table {
+    margin-top: 20px;
+    font-size: 13px;
+  }
 
-.el-tag {
-  margin-left: 10px;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
-.el-tag--default {
-  background-color: #344150;
-}
+  .el-tag {
+    margin-left: 10px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
 
-.button-new-tag {
-  margin-left: 10px;
-  height: 32px;
-  line-height: 30px;
-  padding-top: 0;
-  padding-bottom: 0;
-}
+  .el-tag--default {
+    background-color: #344150;
+  }
 
-.input-new-tag {
-  width: 90px;
-  margin-left: 10px;
-  vertical-align: bottom;
-}
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+
+  .vcenter {
+    display: flex;
+    align-items: center;
+  }
+
 </style>
